ddda337c870694a27b5ce7329917163a8b019043
Motoki Saito <lighttoyork@gmail.com>
2021-10-02 15:37:34 +0900

    ResultList has been added

--- frontend/src/components/pages/Main/Finder/Result/View/index.vue
+++ frontend/src/components/pages/Main/Finder/Result/View/index.vue
@@ -5,14 +5,13 @@
             <v-row>
                 <v-col align="center">
                     <v-img
-                        v-if="!isError"
                         :src="path"
                         max-width="400"
                     />
                 </v-col>
             </v-row>
             <v-divider />
-            <v-simple-table v-if="!isError">
+            <v-simple-table>
                 <template v-slot:default>
                     <tbody>
                         <tr
@@ -25,13 +24,6 @@
                     </tbody>
                 </template>
             </v-simple-table>
-
-            <v-alert 
-                v-if="isError"
-                icon="mdi-alert"
-                type="error"
-            >エラーが発生しました。商品が登録されていないか該当する商品が2つ以上ある可能性があります。
-            </v-alert>
 
             <v-divider />
             <v-row class="pt-7"> 
@@ -50,46 +42,30 @@
 <script>
 export default{
     data: () => ({
-        item:{},
-        isError:false,
         path:''
     }),
-    props:["duct","genre","totalQuery"],
-    methods: {
-        send_query() {
-            const _query = this.totalQuery;
-            this.duct.send(
-                this.duct.nextRid(), 
-                this.duct.EVENT.NEJI,
-                {'genre': this.genre, 'query': _query}
-            );
-        },
-    }, 
+    props:["item"],
     computed:{
         tableData(){
-            const _arr = Object.entries(this.item).map(([key, value]) => ({key, value}));
-            return _arr
+            const _arr = Object.entries(this.item[0]).map(([key, value]) => ({key, value}));
+            let _arr2 = _arr.filter(item => { 
+                if(!isNaN(item.value) || typeof item.value === 'string'){
+                    return true
+                }else{
+                    return false
+                }
+            });
+            return _arr2
         },
     },
     created(){
-        this.duct.invokeOnOpen(async () => {
-            this.duct.setEventHandler(
-                this.duct.EVENT.NEJI,
-                (rid, eid, data) => {
-                    console.log(data);
-                    this.$set(this, 'item', 'items' in data ? data.items[0] : {});
-                    if(!Object.keys(data).includes("items")){
-                        this.isError = true;
-                    }else{
-                        this.isError = false;
-                        console.log(this.tableData);
-                        let _jan = String(this.tableData[0].value);
-                        this.path = require(`@/assets/productsImage/${_jan}.jpg`);
-                    }
-                }
-            )
-            this.send_query();
-        });
+        console.log(this.tableData);
+        let _jan = String(this.tableData[0].value);
+        try{
+            this.path = require(`@/assets/productsImage/${_jan}.jpg`);
+        }catch{
+            this.path = require(`@/assets/productsImage/no_image.jpg`);
+        }
     },
     mounted(){
         this.$emit('add-step', 4);
