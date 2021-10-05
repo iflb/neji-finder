b0386c985e06ad03345004dbc42f780aeaf3a33b
Ubuntu <ubuntu@ip-172-31-39-163.ap-northeast-1.compute.internal>
2021-10-05 02:19:54 +0000

    formatting start buttons and disabling second&third button

--- frontend/src/components/pages/Main/Finder/Start.vue
+++ frontend/src/components/pages/Main/Finder/Start.vue
@@ -2,36 +2,41 @@
     <v-card class="pa-1 ma-1" color="grey lighten-3">
         <v-card-title>検索を開始する</v-card-title>
         <v-card-text> 
-            <v-row class="d-flex justify-center pb-2"> 
-                <v-btn
-                    dark
-                    color="primary"
-                    width="50%"
-                    @click="emitComponentName"
-                >
-                    <v-icon>mdi-magnify</v-icon>
-                    形状から検索
-                </v-btn> 
-            </v-row>
-            <v-row class="d-flex justify-center pb-2"> 
-                <v-btn
-                    dark
-                    width="50%"
-                    color="primary"
-                >
-                    <v-icon>mdi-magnify</v-icon>
-                    規格から検索
-                </v-btn>  
-            </v-row>
-            <v-row class="d-flex justify-center pb-2"> 
-                <v-btn
-                    dark
-                    width="50%"
-                    color="primary"
-                >
-                    <v-icon>mdi-magnify</v-icon>
-                    キーワードから検索
-                </v-btn>  
+            <v-row class="d-flex justify-center pb-2" style="font-size:1.2em"> 
+                <v-col cols="10" md="8">
+                    <v-btn
+                        dark
+                        color="primary"
+                        width="100%"
+                        height="100px"
+                        @click="emitComponentName"
+                    >
+                        <v-icon>mdi-magnify</v-icon>
+                        形状から検索
+                    </v-btn> 
+                </v-col>
+                <v-col cols="10" md="8">
+                    <v-btn
+                        color="primary"
+                        width="100%"
+                        height="100px"
+                        disabled
+                    >
+                        <v-icon>mdi-magnify</v-icon>
+                        規格から検索
+                    </v-btn>  
+                </v-col>
+                <v-col cols="10" md="8">
+                    <v-btn
+                        color="primary"
+                        width="100%"
+                        height="100px"
+                        disabled
+                    >
+                        <v-icon>mdi-magnify</v-icon>
+                        キーワードから検索
+                    </v-btn>  
+                </v-col>
             </v-row>
         </v-card-text>   
 
